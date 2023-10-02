5145406317299b25b4feb20647776e3fbc2fd583
akashpahilwan <akashpahilwan53@gmail.com>
2023-10-02 23:24:14 +0530

    trying to get it right

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -116,7 +116,7 @@ const StyledPic = styled.div`
 const About = () => {
   const revealContainer = useRef(null);
   const prefersReducedMotion = usePrefersReducedMotion();
-  const blogUrl = `${window.location.origin}/pensieve/`;
+  const blogUrl = `/pensieve/`;
 
   useEffect(() => {
     if (prefersReducedMotion) {
